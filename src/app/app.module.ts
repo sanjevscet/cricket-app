f1b42d8546c775f8dd028aa22d2335c2edab48d4
Sanjeev Kumar <Sanjeev.kumar@goavege.com>
2020-06-06 14:05:51 +0530

    Core Template is made

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,60 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactCreateComponent } from './contact-create/contact-create.component';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactCreateComponent,
+    ContactListComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+// import { BrowserModule } from '@angular/platform-browser';
+// import { NgModule } from '@angular/core';
+
+// import { AppComponent } from './app.component';
+// import { LoginComponent } from './login/login.component';
+// import { HomeComponent } from './home/home.component';
+// import { ContactCreateComponent } from './contact-create/contact-create.component';
+// import { ContactListComponent } from './contact-list/contact-list.component';
+// import { HeaderComponent } from './header/header.component';
+// import { FooterComponent } from './footer/footer.component';
+
+// @NgModule({
+//   declarations: [
+//     AppComponent,
+//     LoginComponent,
+//     HomeComponent,
+//     ContactCreateComponent,
+//     ContactListComponent,
+//     HeaderComponent,
+//     FooterComponent
+//   ],
+//   imports: [
+//     BrowserModule
+//   ],
+//   providers: [],
+//   bootstrap: [AppComponent]
+// })
+// export class AppModule { }
